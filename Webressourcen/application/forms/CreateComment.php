4d6bd741b35a20be6379f10361efdb5d4931981c
ChristophB <chrib@hotmail.de>
2012-04-23 21:33:03 +0200

    neues Formular fuer die kommentar-erstellung; muss nach initialisierung mit setIDs( $topicID, $userID, $topicVersion) befuellt werden

---
+++ Webressourcen/application/forms/CreateComment.php
@@ -0,0 +1,21 @@
+<?php
+
+class Application_Form_CreateComment extends Zend_Form
+{
+
+    public function init()
+    {
+        $this->setMethod( 'post');
+        $this->setAttrib( 'action', 'validatecomment');
+        $this->addElement( 'textarea', 'commentText', array( 'label' => 'Kommentar', 'rows' => '10', 'cols' => '50'));
+        $this->addElement( 'submit', 'senden');
+    }
+    
+    public function setIDs( $topicID, $userID, $topicVersion)
+    {
+        $this->addElement( 'hidden', 'topicID', array( 'value' => $topicID));
+        $this->addElement( 'hidden', 'userID', array( 'value' => $userID));
+        $this->addElement( 'hidden', 'topicVersion', array( 'value' => $topicVersion));
+    }
+}
+
