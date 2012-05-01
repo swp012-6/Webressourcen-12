ad195374e07bbf2f4b9302084e767ac205f7f995
ChristophB <chrib@hotmail.de>
2012-05-01 22:30:57 +0200

    submitbutton wird nun erst durch das aufrufen von setTopicID() an das ende der form angefügt

--- Webressourcen/application/forms/CreateFriend.php
+++ Webressourcen/application/forms/CreateFriend.php
@@ -1,5 +1,10 @@
 <?php
-
+/** This form is used to get data for creating a new friend.
+  * First you have to create an instance of this class.
+  * After this, use the function setTopicID() to add the topicID and the submitbutton
+  *
+  * @author Christoph Beger
+  */
 class Application_Form_CreateFriend extends Zend_Form
 {
 
@@ -12,12 +17,15 @@ class Application_Form_CreateFriend extends Zend_Form
         $this->addElement( 'text', 'email', array( 'label' => 'Email-Adresse:'));
         $this->addElement( 'text', 'job', array( 'label' => 'Beruf:'));
         $this->addElement( 'text', 'adresse', array( 'label' => 'Adresse:'));
-        $this->addElement( 'submit', 'neuen Freund einladen');
     }
-    
+   
+    /** adds the topicID and a submitbutton to the form
+      * @param $topicID ID of the topic which will be accessible by the created user
+      */
     public function setTopicID( $topicID)
     {
         $this->addElement( 'hidden', 'topicID', array( 'value' => $topicID));
+        $this->addElement( 'submit', 'neuen Freund einladen');
     }
 
 
