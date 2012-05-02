f9aa889dbcb2670e69baf6f4a142c30d8afd87be
Peter <pkornski@googlemail.com>
2012-05-02 23:06:45 +0200

    BComment ich habe versehentlich die Daten in den falschen Ordner geschoben und überschreiben... jetzt muss ich es nochmal laden -.-

--- BComment/application/models/CommentModel.php
+++ BComment/application/models/CommentModel.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @copyright Copyright (c) 2012, {@link http://aksw.org AKSW}
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License(GPL)
+ */
+
+/**
+ * loades the database table comment
+ */
 class CommentModel extends Zend_Db_Table_Abstract
 {
     protected $_name = 'comment';
