8690d8664aa4234533040c4ed69b81a0e8197d90
ChristophB <chrib@hotmail.de>
2012-05-09 20:32:25 +0300

    radiobutton text bei themenerstellung standardmäßig ausgewählt

--- Webressourcen/application/forms/CreateTopic.php
+++ Webressourcen/application/forms/CreateTopic.php
@@ -8,16 +8,17 @@ class Application_Form_CreateTopic extends Zend_Form
     public function init()
     {
         $this->setMethod( 'post');
-		$this->setAttrib( 'action', 'validate');
-		$this->addElement( 'radio', 'topicType', array(	'label' => 'Typ:',
-                                                        'multioptions' => array(	0 => 'Text',
-                                                                                    1 => 'Link'),
-														'separator' => '  '));
-		$this->addElement( 'text', 'topicName', array( 'label' => 'Topic-Name:'));
-		$this->addElement( 'textarea', 'topicContent', array( 'label' => 'Inhalt:', 'rows' =>20, 'cols' => 90));
+        $this->setAttrib( 'action', 'validate');
+	$this->addElement( 'radio', 'topicType', array(	'label' => 'Typ:',
+                                                        'multioptions' => array( 0 => 'Text',
+                                                                                 1 => 'Link'),
+							'separator' => '  ',
+                                                        'value' => 0 ));
+	$this->addElement( 'text', 'topicName', array( 'label' => 'Topic-Name:'));
+	$this->addElement( 'textarea', 'topicContent', array( 'label' => 'Inhalt:', 'rows' =>20, 'cols' => 90));
         $this->getElement( 'topicContent')->addValidator( new BV_Validate_Uri());
-		$this->addElement( 'text', 'topicSource', array( 'label' => 'Quelle:'));
-		$this->addElement( 'submit', 'erstellen');
+	$this->addElement( 'text', 'topicSource', array( 'label' => 'Quelle:'));
+	$this->addElement( 'submit', 'erstellen');
     }
 
 
